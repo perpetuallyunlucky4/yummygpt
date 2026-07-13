6ddcca899df87a575a837386204c0720970ffeb5
perpetuallyunlucky4 <goldtitaniumalloy72@gmail.com>
2026-07-13 13:21:24 +0800

    Update load_TinyStories.py

--- load_TinyStories.py
+++ load_TinyStories.py
@@ -2,7 +2,7 @@ from datasets import load_dataset
 
 dataset = load_dataset("roneneldan/TinyStories")
 
-tezt = "<|endoftext|>\n".join(dataset["train"]["text"][:12000]) + "<|endoftext|>"
+tezt = "<|endoftext|>\n\n".join(dataset["train"]["text"][:12000]) + "<|endoftext|>"
 
-with open("TinyStories.txt", "w") as f:
+with open("model_train_files/TinyStories.txt", "w") as f:
   f.write(tezt)
